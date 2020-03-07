a9bce46d88a4b4570c4154f59102a0c05c278340
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2020-03-07 20:39:28 -0300

    Implementação da intrução load-|M(X)|. E adição dos comentários para mostrar no 'help'

--- ias/instructions/dataTransfer.py
+++ ias/instructions/dataTransfer.py
@@ -14,21 +14,26 @@ class DataTransfer():
         super().__init__()
     
     def loadMq(self, mq, ac):
+        """00001010 - Transfere o conteúdo de MQ para AC"""
         ac = mq
         return ac
 
     def loadMqMx(self, x, mq, memory):
+        """00001001 - Transfere o conteúdo do local de memória X para MQ"""
         mq = memory[x]
         return mq
     
     def storMx(self, ac, x, memory):
+        """00100001 - Transfere o conteúdo de AC para o local de memória X"""
         memory[x] = ac
 
     def loadMx(self, ac, x, memory):
+        """00000001 - Transfere M(X) para o AC"""
         ac = memory[x]
         return ac
     
     def loadMxNeg(self, ac, x, memory):
+        """00000010 - Transfere – M(X) para o AC"""
         if(memory[x][0] == '1'):
             temp = '0' + memory[x][1:]
         else:
@@ -37,6 +42,13 @@ class DataTransfer():
         return ac
     
     def loadAbsMx(self, ac, x, memory):
+        """00000011 - Transfere o valor absoluto de M(X) para o AC"""
         absMx = '0' + memory[x][1:]
         ac = absMx
+        return ac
+    
+    def loadAbsMxNeg(self, ac, x, memory):
+        """00000100 - Transfere -|M(X)| para o acumulador"""
+        absMxNeg = '1' + memory[x][1:]
+        ac = absMxNeg
         return ac
