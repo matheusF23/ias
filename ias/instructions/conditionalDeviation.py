d0c6b762f52f40fe579897ec310bf828adf11885
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2020-03-07 21:44:33 -0300

    Implementação da instruçãos JUMP+ M(X,0:19)

---
+++ ias/instructions/conditionalDeviation.py
@@ -0,0 +1,19 @@
+## Unconditional Deviation instructions ##
+#
+#  Opcode  | Symbolic representation | Description
+# 00001111 |     JUMP+ M(X,0:19)     | Se o número no AC for não negativo, apanha a próxima instrução da 
+#          |                         | metade esquerda de M(X)
+# 00010000 |     JUMP+ M(X,20:39)    | Se o número no AC for não negativo, apanha a próxima instrução da 
+#          |                         | metade direita de M(X)
+
+class ConditionalDeviation():
+    def __init__(self):
+        super().__init__()
+    
+    def jumpMxLeft(self, ac, x, memory):
+        """00001111 -  Se o número no AC for não negativo, apanha a próxima instrução da metade 
+        esquerda de M(X)
+        """
+        if(ac[0] == '0'):
+            return memory[x][0:19]
+        return ''
