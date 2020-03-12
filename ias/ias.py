e8574adca1607a33bcdce1e70b7e6931c19eda06
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2020-03-12 11:44:01 -0300

    Criação dos registradores

---
+++ ias/ias.py
@@ -0,0 +1,10 @@
+
+
+# Definição dos registradores
+mbr = ''    # Registrador de buffer de memória
+mar = ''    # Registrador de endereço de memória
+ir = ''     # Registrador de instrução
+ibr = ''    # Registrador de buffer de instrução
+pc = ''     # Contador de programa
+ac = ''     # Acumulador
+mq = ''     # Quociente multiplicador
