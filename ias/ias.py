d1da3c0cff2b686e349bf09f1dd06c71a1953dc0
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2020-03-12 13:46:27 -0300

    Instanciando as instruções

--- ias/ias.py
+++ ias/ias.py
@@ -1,4 +1,11 @@
 
+from ias.arithmeticOperations.operations import Operations
+from ias.instructions.arithmetic import Arithmetic
+from ias.instructions.addressModification import AddressModification
+from ias.instructions.conditionalDeviation import ConditionalDeviation
+from ias.instructions.dataTransfer import DataTransfer
+from ias.instructions.unconditionalDeviation import UnconditionalDeviation
+
 # Inicialização da memória
 memory = []
 for i in range(1000):
@@ -12,3 +19,11 @@ ibr = ''    # Registrador de buffer de instrução
 pc = ''     # Contador de programa
 ac = ''     # Acumulador
 mq = ''     # Quociente multiplicador
+
+# Inicialização de Instâncias necessárias
+op = Operations()
+arithmetic = Arithmetic()
+addressModification = AddressModification()
+conditionalDeviation = ConditionalDeviation()
+dataTransfer = dataTransfer()
+unconditionalDeviation = UnconditionalDeviation()
